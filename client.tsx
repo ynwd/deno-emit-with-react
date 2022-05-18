87cac186c34934e389e49e3f6f884f6764a9c303
yanu <10122431+ynwdd@users.noreply.github.com>
2022-05-19 06:39:13 +0700

    remove assert

--- client.tsx
+++ client.tsx
@@ -1,4 +1,4 @@
-import React, { createElement as h } from "https://esm.sh/react@18.1.0";
+import React from "https://esm.sh/react@18.1.0";
 import { createRoot } from "https://esm.sh/react-dom@18.1.0/client";
 import App from "./App.tsx";
 //@ts-ignore:
--- emit.ts
+++ emit.ts
@@ -1,15 +1,6 @@
 import { emit } from "https://deno.land/x/emit@0.0.1/mod.ts";
-import {
-  assert,
-  assertEquals,
-  assertStringIncludes,
-} from "https://deno.land/std@0.138.0/testing/asserts.ts";
 
 const url = new URL("./client.tsx", import.meta.url);
 const result = await emit(url.href);
 
 console.log(result);
-assertEquals(Object.keys(result).length, 1);
-const code = result[Object.keys(result)[0]];
-assert(code);
-assertStringIncludes(code, "export default function hello()");
